7b984997c8e37a0e2154eb434f807a5059212c20
alantrrs <alan.trrs.ngls@gmail.com>
2016-01-12 00:52:57 -0800

    fix lint

--- index.js
+++ index.js
@@ -7,13 +7,12 @@ var SOCKET_PORT = process.env.SOCKET_PORT || 3001
 socket.connect('tcp://127.0.0.1:' + SOCKET_PORT)
 
 socket.subscribe('command')
-console.log('Suscriber connected to port '+ SOCKET_PORT)
+console.log('Suscriber connected to port ' + SOCKET_PORT)
 
-socket.on('message', function(topic, message){
+socket.on('message', function (topic, message) {
   console.log('received a message related to:', topic.toString('utf-8'), 'containing message:', message.toString('utf-8'))
-  exec('docker run hello-world',function(err, stdout, stderr){
-    if (err) throw err;
+  exec('docker run hello-world', function (err, stdout, stderr) {
+    if (err) throw err
     console.log(stdout)
   })
 })
-
