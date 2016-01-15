54d300844aee207eebb03dd61d68f0cf4a470e6a
alantrrs <alan.trrs.ngls@gmail.com>
2016-01-15 01:01:56 -0800

    try to fix entrypoint

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 var emp = require('./lib')
 
+// TODO: Print usage
+// if emp [ params ] [ directory | git url ]
+// if directory is empty then emp is run as a web worker and will connect to empirical.com to wait for tasks
+// emp .
+// emp git@github:alantrrs/emp
+// emp --configure to configure
+
 // TODO: Get params from cli
 var git_repo = 'git@github.com:alantrrs/emp'
 var yml_file = 'empirical.yml'
