16d176c8449e9c4ee278a999c76ab458dd65c194
alantrrs <alan.trrs.ngls@gmail.com>
2016-01-17 16:00:45 -0800

    Setting up CLI mode

--- config/index.js
+++ config/index.js
@@ -3,7 +3,7 @@ var config = {}
 
 config.workspaces = process.env.EMPIRICAL_WORKSPACES || '~/workspaces'
 config.data_dir = process.env.EMPIRICAL_DATA || '~/data'
-
+config.config_filename = 'empirical.yml'
 // TODO: Read it from a config.json
 config.git = {
   auth: {
--- index.js
+++ index.js
@@ -1,23 +1,42 @@
+var config = require('./config')
 var emp = require('./lib')
 
-// TODO: Print usage
+// TODO: Print help
 // if emp [ params ] [ directory | git url ]
-// if directory is empty then emp is run as a web worker and will connect to empirical.com to wait for tasks
+// if directory is empty then emp is run as a worker and will connect to empirical.com to wait for tasks
 // emp .
 // emp git@github:alantrrs/emp
 // emp --configure to configure
 
-// TODO: Get params from cli
-var git_repo = 'git@github.com:alantrrs/emp'
-var yml_file = 'empirical.yml'
+// TODO: Check if it's configured
+// Configure
+// - data path
+// - ssh credentials
+// - tmp sessions path
 
 // TODO: Get random id from the Request
 // var session_id = req.session_id
-emp.createSessionDirs().then(function (dirs) {
-  return emp.cloneRepository(git_repo, dirs.code).then(function () {
-    return emp.buildImage(dirs.code, yml_file)
-  }).then(function () {
-    return emp.runExperiments(dirs.code, yml_file)
+
+
+var args = process.argv
+var experiment = {}
+
+if (args.length > 2) {
+  experiment.cli_mode = true
+  // Last argument should be a directory or URL
+  // Get project dir or URL to repo
+  var git_repo = args[args.length - 1]
+  emp.createSessionDirs().then(function (dirs) {
+    return emp.cloneRepository(git_repo, dirs.code).then(function () {
+      return emp.buildImage(dirs.code, config.config_filename)
+    }).then(function () {
+      return emp.runExperiments(dirs.code, confg.config_filename)
+    })
   })
-})
   .catch(emp.handleError)
+} else {
+  console.log('IMPLEMENT WORKER MODE')
+ // TODO: launch client mode
+}
+
+
