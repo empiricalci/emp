6b90c3d35edeb6e6fd03b410cdde13b23fa091d0
alantrrs <alan.trrs.ngls@gmail.com>
2016-03-15 00:14:11 -0700

    Get ssh credentials from empirical's API

--- config/index.js
+++ config/index.js
@@ -3,20 +3,10 @@ var config = {}
 
 const ENV = process.env.EMPIRICAL_ENV
 
+// TODO: Read from a config.json
 config.workspaces = process.env.EMPIRICAL_WORKSPACES || '~/workspaces'
 config.data_dir = process.env.EMPIRICAL_DATA || '~/data'
 config.config_filename = 'empirical.yml'
-// TODO: Read it from a config.json
-// TODO: Mount keys into docker container
-config.git = {
-  auth: {
-    type: 'ssh',
-    user: 'alantrrs',
-    privateKey: '/home/ambidextrvs/.ssh/id_rsa',
-    pubKey: '/home/ambidextrvs/.ssh/id_rsa.pub',
-    passphrase: ''
-  }
-}
 
 config.amqp = {
   host: process.env.EMPIRICAL_AMQP_URL || 'amqp://empirical-queue',
