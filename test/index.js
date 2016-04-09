7c717ffbee927a53b196c30eeeca34ec9b217b43
alantrrs <alan.trrs.ngls@gmail.com>
2016-04-09 01:14:33 -0700

    Don't skip push test

--- test/index.js
+++ test/index.js
@@ -3,42 +3,24 @@
 var assert = require('assert')
 var fetch = require('node-fetch')
 
-var docker = require('../lib/docker')
-
 function onProgress (data) {
   console.log(data)
 }
 
-before(function (done) {
-  // Wait for the server to be up
-  this.timeout(60000)
-  console.log('Waiting for the server to be up')
-  function waitForIt() {
-    console.log('.')
-    return fetch(process.env.EMPIRICAL_API_URI).then(function (res) {
-      if (res.ok) return done()
-      setTimeout(waitForIt,2000)
-    }).catch(function (err) {
-      setTimeout(waitForIt,2000)
-    })
-  }
-  waitForIt()
-})
-
+var docker = require('../lib/docker')
 describe('Docker', function () {
   it('should pull', function (done) {
     this.timeout(20000)
-    docker.pull('tianon/true')
+    docker.pull('empiricaladmin/true')
     .then(function (data) {
       done()
     }).catch(function (err) {
       done(err)
     })
   })
-  // TODO: Get a small image and re-tag it first to test this
-  it.skip('should push an image', function (done) {
+  it('should push an image', function (done) {
     this.timeout(10000)
-    docker.push('empiricaladmin/empirical-bot-my-solver')
+    docker.push('empiricaladmin/true')
     .then(function (data) {
       done()
     }).catch(function (err) {
@@ -50,6 +32,17 @@ describe('Docker', function () {
 var client = require('../lib/client')
 
 describe('Client', function () {
+  before(function (done) {
+    this.timeout(30000)
+    function waitForIt() {
+      return fetch(process.env.EMPIRICAL_API_URI).then(function (res) {
+        return done()
+      }).catch(function (err) {
+        setTimeout(waitForIt,2000)
+      })
+    }
+    waitForIt()
+  })
   it('updates a build', function (done) {
     client.updateBuild({
       _id : "56f5da4e49b2e7531469c3ef",
