0a1ab0f8144003a8b5baca57c770540edc191c01
alantrrs <alan.trrs.ngls@gmail.com>
2016-04-11 16:29:12 -0700

    git it 5mins

--- test/client.js
+++ test/client.js
@@ -44,7 +44,7 @@ describe('emp', function () {
     waitForIt(done)
   })
   it('builds an evaluator', function (done) {
-    this.timeout(120000)
+    this.timeout(300000)
     emp.runTask(builds.evaluator).then(function () {
       // TODO: Assert the image exists
       done()
