ce284b1be0f235027f5246230bb68b8a3310e653
Alan <alan.trrs.ngls@gmail.com>
2016-07-20 18:27:57 -0700

    Release 0.1.1 (#55)
    * Patch run script
    * Fix install script

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,8 @@
 # Changelog
 
+## v0.1.1 (20 Jul 2016)
+- Adds full version to launch script ``./bin/run.sh``
+- Fix install script
+
 ## v0.1.0 (20 Jul 2016)
 - First alpha release candidate
--- install.sh
+++ install.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-set -e
-
 # Download
 DEST=/usr/local/bin/emp
 VERSION=$(curl -s https://api.github.com/repos/empiricalci/emp/tags | grep -Eo '"name":.*?[^\\]",'  | head -n 1 | sed 's/[," ]//g' | cut -d ':' -f 2)
-curl -sL https://raw.githubusercontent.com/empiricalci/emp/$VERSION/bin/run.sh -o $DEST
-
+curl -sSL https://raw.githubusercontent.com/empiricalci/emp/$VERSION/bin/run.sh -o $DEST
 # Make executable
-chmod +x $DEST
+if [ "$?" = "0" ] && [ -f $DEST ]; then
+  chmod +x $DEST
+  echo "Installed emp $VERSION to $DEST"
+else
+  echo "Download failed. Run with sudo."
+fi
