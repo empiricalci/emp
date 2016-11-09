2e20b7652e3464f5add65494445a4029e41c26bd
alantrrs <alan.trrs.ngls@gmail.com>
2016-11-08 18:45:57 -0800

    Remove unused file

--- bin/run.bat
+++
@@ -1,9 +0,0 @@
-if -%1-==-- echo Use: emp experiment-name path-to-code & exit /b
-if -%2-==-- echo Use: emp experiment-name path-to-code & exit /b
-docker run -t --rm -v /var/run/docker.sock:/var/run/docker.sock^
- -v %2:/empirical/code:ro^
- -v %EMPIRICAL_DIR%/data:/empirical/data^
- -v %EMPIRICAL_DIR%/workspaces:/empirical/workspaces^
- -e EMPIRICAL_DIR=%EMPIRICAL_DIR%^
- -e DEBUG=dataset-cache^
- empiricalci/emp %1
